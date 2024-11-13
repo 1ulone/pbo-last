7e05ab148d80a3415a04824640bf32832d550450
Shacio21 <dirgammuhammad@gmail.com>
2024-11-13 11:29:41 +0700

    Models&Controlleer
    bimbingan,feedback,seminar,sidang

--- ta-app/app/Models/Feedback.php
+++ ta-app/app/Models/Feedback.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Feedback extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'detailfeedback';
+
+    protected $fillable = [
+        'mahasiswa_id',
+        'bimbingan_id',
+        'seminar_id',
+        'sidang_id',
+        'detail'
+    ];
+
+    // Define relationships if needed
+    public function pembimbing()
+    {
+        return $this->belongsTo(User::class, 'pembimbing_id');
+    }
+
+    public function penguji()
+    {
+        return $this->belongsTo(User::class, 'penguji_id');
+    }
+
+    public function mahasiswa()
+    {
+        return $this->belongsTo(User::class, 'mahasiswa_id');
+    }
 }
--- ta-app/app/Models/Seminar.php
+++ ta-app/app/Models/Seminar.php
@@ -2,9 +2,35 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Seminar extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'seminar';
+
+    protected $fillable = [
+        'pembimbing_id',
+        'penguji_id',
+        'mahasiswa_id',
+        'jadwal_bimbingan'
+    ];
+
+    // Define relationships if needed
+    public function pembimbing()
+    {
+        return $this->belongsTo(User::class, 'pembimbing_id');
+    }
+
+    public function penguji()
+    {
+        return $this->belongsTo(User::class, 'penguji_id');
+    }
+
+    public function mahasiswa()
+    {
+        return $this->belongsTo(User::class, 'mahasiswa_id');
+    }
 }
