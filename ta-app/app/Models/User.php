feb82b6d20682d758c2d02bad8b6021188b70f39
evragg <evraridjki77@gmail.com>
2024-11-13 12:52:39 +0700

    Users
    Update User Models

--- ta-app/app/Models/User.php
+++ ta-app/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+//combsd
 class User extends Model
 {
     protected $table = 'users';
